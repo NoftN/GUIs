6a081585141cf6cbf6172bd86d760fc135b0b9b3
NoftN <102395817+NoftN@users.noreply.github.com>
2022-03-26 09:50:45 +0800

    Update GUI.lua

--- GUI.lua
+++ GUI.lua
@@ -1,4 +1,4 @@
-local discinv = 
+local discinv = "https://discord.gg/qTmbWQ2MMa"
 
 local getGame = pcall(function()
     loadstr = game:HttpGet("https://github.com/NoftN/GUIs"..game.PlaceId..".lua")
@@ -7,4 +7,6 @@ if getGame == true then
     loadstring(loadstr)()
 else
     game.Players.LocalPlayer:Notify("The game you're playing is not supported. ")
+    game.Players.LocalPlayer:Notify("Discord Invite has been copied to clipboard.")
+    setclipboard(discinv)
 end
